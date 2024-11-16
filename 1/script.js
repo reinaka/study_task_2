117d302d9843e45624d9b7a2c9e11a06755e3b35
reinaka <juliabednaia@gmail.com>
2024-11-16 13:05:58 +0300

    change api for slider, fix some styles

--- 1/script.js
+++ 1/script.js
@@ -1,3 +1,4 @@
+const key = "live_VM8M2pKntzkdC5jaHej95TMq5kPpo1nHRjtrZlZC0lGopHyFwyBYSXplGiHr1wyc";
 // Функция для получения фотографий
 async function getImages(url) {
     try {
@@ -12,13 +13,12 @@ function renderSlides(imageList, container) {
     for(let i = 0; i < imageList.length; i++) {
         const slide = document.createElement("div");
         slide.classList.add(`slide`);
-        slide.innerHTML = `<img class="image image--${i}" src=${imageList[i].download_url} alt="image ${i + 1}"/><h2 class="slide__heading">Slide ${i + 1}</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div>`;
+        slide.innerHTML = `<div class="image__wrapper"><img class="image image--${i}" src=${imageList[i].url} alt="image ${i + 1}"/></div><h2 class="slide__heading">Slide ${i + 1}</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div>`;
         container.append(slide);
     }
 }
 
-const imageList = await getImages("https://picsum.photos/v2/list?page=1&limit=9");
-console.log(imageList);
+const imageList = await getImages(`https://api.thecatapi.com/v1/images/search?limit=9&api_key=${key}`);
 renderSlides(imageList, document.querySelector(".slider__track"));
 
 
